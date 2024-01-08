5823034020ad376538dc3248b7d240f98e6ff6c1
DESKTOP-FIA3LA0 <jawaddl2007@Gmail.com>
2024-01-08 22:57:42 +0200

    mission1

--- app/src/main/java/com/example/jawaddallashe_mission/MainActivity.java
+++ app/src/main/java/com/example/jawaddallashe_mission/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.jawaddallashe_mission;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
 
+    }
 
+    public void go(View view) {
+        btn.setText("Oh, yea, I’ve been clicked!");
+        btn.setTextSize(20);
     }
 }
